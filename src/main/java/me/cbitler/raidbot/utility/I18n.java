c7afa9cdca2415dc9233034ec3713060ce924b4a
Matthieu <matt.pauze@gmail.com>
2022-01-21 13:26:01 -0500

    Finally moved properties from .properties files to .java classes. Hopefully still works after deployment

--- src/main/java/me/cbitler/raidbot/utility/I18n.java
+++ src/main/java/me/cbitler/raidbot/utility/I18n.java
@@ -7,28 +7,28 @@ import java.util.ResourceBundle;
 public class I18n {
     private static ResourceBundle bundle;
     private final static String BUNDLE_NAME = "me.cbitler.raidbot.res.MessageBundle";
-    private I18n(){
+
+    private I18n() {
     }
 
     public static Locale getLocale() {
         return Locale.getDefault();
     }
 
-    public static boolean isSupported(Locale l){
+    public static boolean isSupported(Locale l) {
         return Arrays.asList(Locale.getAvailableLocales()).contains(l);
     }
 
-    public static void setLocale(Locale l){
+    public static void setLocale(Locale l) {
         Locale.setDefault(l);
     }
 
     public static String getMessage(String key) {
         if (bundle == null) {
-            bundle = ResourceBundle.getBundle(BUNDLE_NAME, Locale.ENGLISH);
+            bundle = ResourceBundle.getBundle(BUNDLE_NAME);
         }
         return bundle.getString(key);
     }
-
 
 
 }
